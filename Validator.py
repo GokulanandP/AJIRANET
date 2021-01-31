5d76b73152ae52a72ff7131007fddc19a5c2c221
GOKULANAND P <pgokulanand16@gmail.com>
2021-01-31 22:41:14 +0530

    All the functions in validator file has been reduced to functions with less than 35 lines for readability

--- Validator.py
+++ Validator.py
@@ -5,60 +5,61 @@ class Validator:
         self.route = ''
         self.return_data = {"status" : True,"message" : ""}
 
-    def Connector(self,data, data_recieved):
-        return_data = {"status": True, "message": ""}
+    def connector_data_validator(self,data,data_recieved):
         try:
             device_names = []
             for devices in data:
                 device_names.append(devices['name'])
             if data_recieved['source'] not in device_names:
-                return_data['status'] = False
-                return_data['message'] = "Node " + data_recieved['source'] + " not found"
-                return return_data
-
+                self.return_data['status'] = False
+                self.return_data['message'] = "Node " + data_recieved['source'] + " not found"
+                return False
             for targets in data_recieved['targets']:
                 if targets not in device_names:
-                    return_data['status'] = False
-                    return_data['message'] = "Node " + targets + " not found"
-                    return return_data
+                    self.return_data['status'] = False
+                    self.return_data['message'] = "Node " + targets + " not found"
+                    return False
+            return True
         except:
-            return_data['status'] = False
-            return_data['message'] = "Invalid command syntax"
-            return return_data
+            self.return_data['status'] = False
+            self.return_data['message'] = "Invalid command syntax"
+            return False
 
-        for targets in data_recieved['targets']:
-            if data_recieved['source'] == targets:
-                return_data['status'] = False
-                return_data['message'] = "Cannot connect device to itself"
-                return return_data
+    def Connector(self, data, data_recieved):
+        if self.connector_data_validator(data,data_recieved):
+            for targets in data_recieved['targets']:
+                if data_recieved['source'] == targets:
+                    self.return_data['status'] = False
+                    self.return_data['message'] = "Cannot connect device to itself"
+                    return self.return_data
 
-        for device in data:
-            if device['name'] == data_recieved['source']:
-                if len(device['targets']) == 0:
-                    device['targets'] = data_recieved['targets']
+            for device in data:
+                if device['name'] == data_recieved['source']:
+                    if len(device['targets']) == 0:
+                        device['targets'] = data_recieved['targets']
+                        for i in data_recieved['targets']:
+                            for device1 in data:
+                                if device1['name'] == i:
+                                    device1['targets'] = device1['targets']+[data_recieved['source']]
+                        self.return_data['status'] = True
+                        self.return_data['message'] = "Successfully connected"
+                        return self.return_data
+                    for i in data_recieved['targets']:
+                        if i in device['targets']:
+                            self.return_data['status'] = False
+                            self.return_data['message'] = "Devices are already connected"
+                            return self.return_data
 
                     for i in data_recieved['targets']:
                         for device1 in data:
                             if device1['name'] == i:
-                                device1['targets'] = device1['targets']+[data_recieved['source']]
-
-                    return_data['status'] = True
-                    return_data['message'] = "Successfully connected"
-                    return return_data
-                for i in data_recieved['targets']:
-                    if i in device['targets']:
-                        return_data['status'] = False
-                        return_data['message'] = "Devices are already connected"
-                        return return_data
-
-                for i in data_recieved['targets']:
-                    for device1 in data:
-                        if device1['name'] == i:
-                            device1['targets'] = device1['targets'] + [data_recieved['source']]
-                device['targets'] = device['targets'] + data_recieved['targets']
-                return_data['status'] = True
-                return_data['message'] = "Successfully connected"
-                return return_data
+                                device1['targets'] = device1['targets'] + [data_recieved['source']]
+                    device['targets'] = device['targets'] + data_recieved['targets']
+                    self.return_data['status'] = True
+                    self.return_data['message'] = "Successfully connected"
+                    return self.return_data
+            else:
+                return self.return_data
 
     # Updating the strength of the device
     def Update_device(self,data,device_to_be_updated,data_recieved):
@@ -160,68 +161,70 @@ class Validator:
                 self.return_data['message'] = 'Successfully added '+ device_detail['name']
                 return self.return_data
 
-
-    def Check_path(self, data, source, destination):
-        return_data = {}
+    def check_path_data_validator(self, data, source, destination):
         if source == None or destination == None:
-            return_data['status'] = False
-            return_data['message'] = 'Invalid Request'
-            return return_data
-
+            self.return_data['status'] = False
+            self.return_data['message'] = 'Invalid Request'
         device_names = []
         for devices in data:
             device_names.append(devices['name'])
         if source not in device_names:
-            return_data['status'] = False
-            return_data['message'] = "Node " + source + " not found"
-            return return_data
+            self.return_data['status'] = False
+            self.return_data['message'] = "Node " + source + " not found"
+            return False
         if destination not in device_names:
-            return_data['status'] = False
-            return_data['message'] = "Node " + destination + " not found"
-            return return_data
-        if source == destination:
-            return_data['status'] = True
-            return_data['message'] = 'Route '+ source + ' -> '+destination
-            return return_data
-        else:
-            source_flag = False
-            destination_flag = False
-            for device in data:
-                if device['name'] == source and device['type'] == 'COMPUTER':
-                    source_flag = True
-                if device['name'] == destination and device['type'] == 'COMPUTER':
-                    destination_flag = True
-            if source_flag == True and destination_flag == True:
-                for device in data:
-                    if device['name'] == source:
-                        if destination in device['targets']:
-                            return_data['status'] = True
-                            return_data['message'] = 'The route is '+source+' -> '+destination
-                            return return_data
-                        route_map = [source]
-                        self.route = source
-                        for targets in device['targets']:
-                            self.route = self.route + ' -> ' + targets
-                            route_map.append(targets)
-                            recursive_function_result = self.recursive_function(data, len(data)-1, targets,
-                                                                                device['strength']-1,
-                                                                                destination, route_map)
-                            print(recursive_function_result)
-                            if recursive_function_result:
-                                return_data['status'] = True
-                                return_data['message'] = 'Route is '+ self.route
-                                return return_data
-                            self.route = self.route.replace(' -> ' + targets, '')
-                            remove = (4 + len(targets)) * -1
-                            self.route = self.route[:remove]
-                            route_map.remove(targets)
-                        return_data['status'] = False
-                        return_data['message'] = 'Route not found'
-                        return return_data
+            self.return_data['status'] = False
+            self.return_data['message'] = "Node " + destination + " not found"
+            return False
+        return True
+
+
+    def Check_path(self, data, source, destination):
+        if self.check_path_data_validator( data, source, destination):
+            if source == destination:
+                self.return_data['status'] = True
+                self.return_data['message'] = 'Route '+ source + ' -> '+destination
+                return self.return_data
             else:
-                return_data['status'] = False
-                return_data['message'] = 'Route cannot be calculated for repeater'
-                return return_data
+                source_flag = False
+                destination_flag = False
+                for device in data:
+                    if device['name'] == source and device['type'] == 'COMPUTER':
+                        source_flag = True
+                    if device['name'] == destination and device['type'] == 'COMPUTER':
+                        destination_flag = True
+                if source_flag == True and destination_flag == True:
+                    for device in data:
+                        if device['name'] == source:
+                            if destination in device['targets']:
+                                self.return_data['status'] = True
+                                self.return_data['message'] = 'The route is '+source+' -> '+destination
+                                return self.return_data
+                            route_map = [source]
+                            self.route = source
+                            for targets in device['targets']:
+                                self.route = self.route + ' -> ' + targets
+                                route_map.append(targets)
+                                recursive_function_result = self.recursive_function(data, len(data)-1, targets,
+                                                                                    device['strength']-1,
+                                                                                    destination, route_map)
+                                if recursive_function_result:
+                                    self.return_data['status'] = True
+                                    self.return_data['message'] = 'Route is '+ self.route
+                                    return self.return_data
+                                self.route = self.route.replace(' -> ' + targets, '')
+                                remove = (4 + len(targets)) * -1
+                                self.route = self.route[:remove]
+                                route_map.remove(targets)
+                            self.return_data['status'] = False
+                            self.return_data['message'] = 'Route not found'
+                            return self.return_data
+                else:
+                    self.return_data['status'] = False
+                    self.return_data['message'] = 'Route cannot be calculated for repeater'
+                    return self.return_data
+        else:
+            return self.return_data
 
     def recursive_function(self, data, length_of_the_graph, current_device, strength, destination,route_map):
         if strength > 0 and length_of_the_graph > 0:
